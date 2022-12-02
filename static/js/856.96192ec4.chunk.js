6822b6b7c82bc0dae69a0f049c99a10a229422cd
valentin-VB <103194587+valentin-VB@users.noreply.github.com>
2022-12-02 20:42:38 +0000

    Deploying to gh-pages from @ valentin-VB/Moviemania@cf3c97146e022d2fd4d5d83ec576e8a48510e0a8 🚀

---
+++ static/js/856.96192ec4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app_template=self.webpackChunkreact_app_template||[]).push([[856],{6856:function(t,a,e){e.d(a,{lrP:function(){return r}});var n=e(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M17.77 3.77L16 2 6 12l10 10 1.77-1.77L9.54 12z"}}]})(t)}}}]);
+//# sourceMappingURL=856.96192ec4.chunk.js.map
